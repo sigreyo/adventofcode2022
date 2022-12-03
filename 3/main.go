70c534945397ad9b43e20c38b675f3e2e7815c81
Your Name Here <your_email@youremail.com>
2022-12-03 17:54:03 +0100

    3.2 done

--- 3/main.go
+++ 3/main.go
@@ -21,20 +21,41 @@ func main() {
 
 	totalScore := 0
 
+	// for scanner.Scan() {
+
+	// 	strLen := len(scanner.Text())
+
+	// 	firstHalf := strings.Split(scanner.Text()[:strLen/2], "")
+	// 	// secondHalf := strings.Split(scanner.Text()[strLen/2:], "")
+	// 	secondHalf := scanner.Text()[strLen/2:]
+
+	// 	for index := range firstHalf {
+	// 		if strings.Contains(secondHalf, firstHalf[index]) {
+	// 			totalScore += prioScore(firstHalf[index])
+	// 			break
+	// 		}
+	// 	}
+
+	// }
+	counter := 1
+	var temp []string
 	for scanner.Scan() {
 
-		strLen := len(scanner.Text())
+		if counter%3 == 0 {
+			badge := strings.Split(scanner.Text(), "")
 
-		firstHalf := strings.Split(scanner.Text()[:strLen/2], "")
-		// secondHalf := strings.Split(scanner.Text()[strLen/2:], "")
-		secondHalf := scanner.Text()[strLen/2:]
-
-		for index := range firstHalf {
-			if strings.Contains(secondHalf, firstHalf[index]) {
-				totalScore += prioScore(firstHalf[index])
-				break
+			for index := range badge {
+				if strings.Contains(temp[0], badge[index]) && strings.Contains(temp[1], badge[index]) {
+					totalScore += prioScore(badge[index])
+					temp = nil
+					break
+				}
 			}
+		} else {
+			temp = append(temp, scanner.Text())
 		}
+
+		counter++
 
 	}
 
